6bc7236916b2ae92b2eed4162ee16282c475391b
Kristina <62182634+kristep@users.noreply.github.com>
2020-09-04 13:18:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class DevConfig(BaseConfig):
 
 class ProdConfig(BaseConfig):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgres://kzstqwqwlpavnh:cb8c327321d6364375a3a27f43c6a0d617a5165b786c3de600271adac1f4fe31@ec2-3-223-9-166.compute-1.amazonaws.com:5432/d8gjmdanvqjid8'
+    SQLALCHEMY_DATABASE_URI = 'postgres://...'
 
 
 class HerokuConfig(BaseConfig):
